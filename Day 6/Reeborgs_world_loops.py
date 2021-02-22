ca2f51daaf20ee69ede7075ed5c520c52d990135
Togun Mayowa <75736590+TMayowa@users.noreply.github.com>
2021-02-22 23:46:57 +0100

    Create Reeborgs_world_loops.py

---
+++ Day 6/Reeborgs_world_loops.py
@@ -0,0 +1,3 @@
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+practiced my loops using the link above. The difficulty and type of challenge can be changed.
