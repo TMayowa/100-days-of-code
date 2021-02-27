f494740df705d080736d6ff0b7754fa70645d962
Togun Mayowa <75736590+TMayowa@users.noreply.github.com>
2021-02-27 23:48:00 +0100

    Create calculator.py

---
+++ Day 10/calculator.py
@@ -0,0 +1,47 @@
+from art import logo
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1,n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1,n2):
+  return n1/n2
+
+operations = {
+  "+":add,
+  "-":subtract,
+  "*":multiply,
+  "/":divide
+  }
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What is the first number?: "))
+
+  for key in operations:
+    print(key)
+  should_continue = True
+
+
+  while should_continue:
+    operation_symbol = input("pick an operation from the line above: ")
+    num2 = float(input("What is the second number?: "))
+
+    function = operations[operation_symbol]
+    answer = function(num1,num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    continue_calc = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+    if continue_calc == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
